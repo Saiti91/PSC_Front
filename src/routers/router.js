565dae052776b6dde08f229f22d02ccedcd8829e
HomeDesktop <vellastephane91@gmail.com>
2024-05-25 09:30:05 +0200

    update

--- src/routers/router.js
+++ src/routers/router.js
@@ -2,22 +2,35 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginPage from '../vues/LoginPage.vue';
 import AccueilPage from '../vues/AccueilPage.vue';
-import AccueilAdminPage from '../vues/administrationVues/AcceuilAdminPage.vue';
-import ServiceAdminPage from '../vues/administrationVues/servicesAdminPage.vue';
-import UserAdminPage from '../vues/administrationVues/usersAdminPage.vue';
-import { authGuard } from '/src/components/Auth/AuthGuard.js';
-
-const routes = [
-    { path: '/', name: 'Login', component: LoginPage },
-    { path: '/accueil', name: 'Accueil', component: AccueilPage },
-    { path: '/accueil-admin', name: 'AccueilAdmin', component: AccueilAdminPage, beforeEnter: authGuard('admin') },
-    { path: '/service-admin', name: 'ServiceAdmin', component: ServiceAdminPage, beforeEnter: authGuard('admin') },
-    { path: '/user-admin', name: 'UserAdmin', component: UserAdminPage, beforeEnter: authGuard('admin') }
-];
+import AccueilAdminPage from '/src/vues/administrationVues/AccueilAdminPage.vue';
+import ServiceAdminPage from '/src/vues/administrationVues/ServicesAdminPage.vue';
+import UserAdminPage from '/src/vues/administrationVues/UsersAdminPage.vue';
+import useAuthGuard from '/src/components/Auth/AuthGuard.js';
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes: [
+        { path: '/', name: 'Login', component: LoginPage },
+        { path: '/accueil', name: 'Accueil', component: AccueilPage },
+        {
+            path: '/accueil-admin',
+            name: 'AccueilAdmin',
+            component: AccueilAdminPage,
+            beforeEnter: useAuthGuard(['admin'])
+        },
+        {
+            path: '/service-admin',
+            name: 'ServiceAdmin',
+            component: ServiceAdminPage,
+            beforeEnter: useAuthGuard(['admin'])
+        },
+        {
+            path: '/user-admin',
+            name: 'UserAdmin',
+            component: UserAdminPage,
+            beforeEnter: useAuthGuard(['admin'])
+        }
+    ]
 });
 
 export default router;
