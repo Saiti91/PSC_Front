c7e5b054291430dc88acd4de04d30158cff7a20f
ambre21 <ambre.laurent21@gmail.com>
2024-07-09 00:34:28 +0200

    Update page MyAccountPage.vue

--- src/vues/clientsVues/accountVues/MyAccountPage.vue
+++ src/vues/clientsVues/accountVues/MyAccountPage.vue
@@ -5,42 +5,75 @@ import FooterComponent from '/src/components/FooterComponent.vue'
 import 'semantic-ui-css/semantic.min.css';
 import {onMounted, ref} from "vue";
 import axiosInstance from "@/utils/Axios.js";
+import Cookies from "js-cookie";
+import VueJwtDecode from 'vue-jwt-decode';
+import Swal from "sweetalert2";
+
+const token = Cookies.get('token');
+
+// Trouvez comment faire marcher cette fonction :
+// - régler pb droit pour suppression
+// - voir comment on fait pour que ça marche avec le bouton
+// - s'assurer que si l'utilisateur est proprio d'un bien il ne puisse pas supprimer lui-même son compte
+const deleteUser = async () => {
+  error.value = null;
+  const decodedToken = VueJwtDecode.decode(token);
+  console.log(decodedToken)
+  try {
+    const response = await axiosInstance.delete(`/users/${decodedToken.uid}/`);
+    if (response.status !== 200) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    user.value = response.data;
+    console.log(user.value)
+    console.log(response.data)
+  } catch (err) {
+    error.value = err.message;
+  }
+};
 </script>
 
 <template>
-  <div class="account-page">
-    <div class="menu-container">
-      <HeaderComponent />
-      <div class="account-menu">
-        <AccountMenuComponent />
-        <div class="account-content">
-          <router-view />
+  <div>
+    <HeaderComponent />
+    <div class="account-container">
+      <AccountMenuComponent />
+      <div class="content-container">
+        <div class="content">
+          <h2>{{ $t('left-title') }}</h2>
+          <p>{{ $t('left-txt') }}</p>
+          <button  class="ui button primary" type="submit">{{ $t('left-btn') }}</button>
         </div>
       </div>
-      <div class="spacer"></div>
     </div>
-  <p>général</p>
     <FooterComponent />
   </div>
 </template>
 
-<style scoped>
-.spacer {
-  margin-top: 7%;
-}
 
-.menu-container {
+<style scoped>
+.account-container {
+  display: flex;
   margin-top: 5%;
 }
 
-.account-page {
-  display: flex;
+.content-container {
+  flex: 1;
+  padding: 20px;
 }
 
-.account-content {
-  margin-left: 20%; /* Compense la largeur du menu */
+.content {
+  background: #fff;
   padding: 20px;
-  width: 80%;
-  overflow-y: auto; /* Ajoute un défilement si nécessaire */
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.field {
+  margin-bottom: 15px;
+}
+
+footer {
+  margin-top: auto;
 }
 </style>
